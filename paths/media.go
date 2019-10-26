e2deac589ad504a0cfa0bf0c7a26b6f5c5eb4d15
Alex Safatli <safatli@cs.dal.ca>
2019-10-25 23:17:57 -0300

    Ignore hidden files in walk

--- paths/media.go
+++ paths/media.go
@@ -70,8 +70,8 @@ func IsVideoFile(basename string) bool {
 func WalkRootDirectory(root string) (err error, paths []MediaLocation) {
 	var cleanRoot = filepath.Clean(root)
 	err = filepath.Walk(cleanRoot, func(path string, info os.FileInfo, err error) error {
-		if err != nil || path == cleanRoot {
-			return nil
+		if err != nil || path == cleanRoot || strings.HasPrefix(info.Name(), ".") {
+			return nil // ignore hidden files, etc.
 		}
 		if info.IsDir() && cleanRoot != filepath.Dir(path) {
 			var loc = MediaLocation{
