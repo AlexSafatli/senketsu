c7c87aa7e28484a64b789445d6e41cb14aeeceb0
Alex Safatli <safatli@cs.dal.ca>
2019-10-29 17:33:54 -0300

    Refactor paths package slightly

--- paths/media.go
+++ paths/media.go
@@ -1,7 +1,6 @@
 package paths
 
 import (
-	"os"
 	"path/filepath"
 	"strings"
 )
@@ -23,14 +22,13 @@ const (
 )
 
 type MediaLocation struct {
-	MediaType      uint8    `json:"-"`
-	RootPath       string   `json:"Path"`
-	Name           string   `json:"Name"`
-	Size           float64  `json:"Size"`
-	ScrapeName     string   `json:"Scrapes To,omitempty"`
-	NumberSeasons  uint     `json:"Number of Seasons,omitempty"`
-	NumberEpisodes uint     `json:"Number of Episodes,omitempty"`
-	MediaFiles     []string `json:"-"`
+	MediaType      uint8   `json:"-"`
+	RootPath       string  `json:"Path"`
+	Name           string  `json:"Name"`
+	Size           float64 `json:"Size"`
+	ScrapeName     string  `json:"Scrapes To,omitempty"`
+	NumberSeasons  uint    `json:"Number of Seasons,omitempty"`
+	NumberEpisodes uint    `json:"Number of Episodes,omitempty"`
 }
 
 type MediaLocationsSplit struct {
@@ -69,52 +67,6 @@ func IsVideoFile(basename string) bool {
 		ext == "ts" || ext == "mov" || ext == "qt" || ext == "wmv" ||
 		ext == "amv" || ext == "mp4" || ext == "m4v" || ext == "mpg" ||
 		ext == "mpeg"
-}
-
-func WalkRootDirectory(root string) (err error, paths []MediaLocation) {
-	var cleanRoot = filepath.Clean(root)
-	err = filepath.Walk(cleanRoot, func(path string, info os.FileInfo, err error) error {
-		if err != nil || path == cleanRoot || strings.HasPrefix(info.Name(), ".") {
-			return nil // ignore hidden files, etc.
-		}
-		if info.IsDir() && cleanRoot != filepath.Dir(path) {
-			var loc = MediaLocation{
-				Name:      strings.ReplaceAll(info.Name(), "\\ ", " "),
-				RootPath:  path,
-				MediaType: GetMediaType(filepath.Dir(path)),
-			}
-			if err := WalkLocationDirectory(&loc); err != nil {
-				return nil
-			}
-			paths = append(paths, loc)
-		} else if cleanRoot == filepath.Dir(path) {
-			return nil
-		}
-		return filepath.SkipDir
-	})
-	return
-}
-
-func WalkLocationDirectory(loc *MediaLocation) (err error) {
-	var seasonCount uint = 0
-	var episodeCount uint = 0
-	err = filepath.Walk(loc.RootPath, func(path string, info os.FileInfo, err error) error {
-		if err != nil || path == loc.RootPath || strings.HasPrefix(info.Name(), ".") {
-			return nil // ignore hidden files, etc.
-		}
-		if loc.MediaType == MediaTypeTV || loc.MediaType == MediaTypeAnime || loc.MediaType == MediaTypeAsianDrama {
-			if info.IsDir() && strings.Contains(info.Name(), "Season") {
-				seasonCount++
-			} else if !info.IsDir() && IsVideoFile(info.Name()) {
-				episodeCount++
-			}
-		}
-		loc.Size += float64(info.Size()) / 1e9
-		return nil
-	})
-	loc.NumberEpisodes = episodeCount
-	loc.NumberSeasons = seasonCount
-	return
 }
 
 func SplitIntoMediaTypes(locations []MediaLocation) MediaLocationsSplit {
